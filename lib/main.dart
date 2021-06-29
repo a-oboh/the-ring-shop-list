0094328d368b324ab6a3ea8b2fac7c80b74684df
Albert Oboh <albertoboh7@gmail.com>
2021-06-29 22:01:06 +0100

    create service locator with get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'service_locator.dart' as di;
 
-void main() {
+void main() async{
+  //initialize dependency injection
+  await di.init();
+  
   runApp(MyApp());
 }
 
