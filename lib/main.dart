b88c4680512dcbd617adfb5d27bc6c1748b76f9a
Albert Oboh <albertoboh7@gmail.com>
2021-07-01 23:00:15 +0100

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'service_locator.dart' as di;
 
-void main() async{
+void main() async {
   //initialize dependency injection
   await di.init();
 
-  runApp(ShoeList());
+  runApp(ProviderScope(child: ShoeList()));
 }
 
 class ShoeList extends StatelessWidget {
@@ -22,7 +23,7 @@ class ShoeList extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, this.title}) : super(key: key);
 
   final String? title;
 
@@ -49,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
@@ -62,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
