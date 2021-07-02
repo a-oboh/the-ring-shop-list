3cbf60a1d361c36233929aa7ac9dc0df9432efaf
Albert Oboh <albertoboh7@gmail.com>
2021-07-03 00:24:46 +0100

    create shop list freezed class

---
+++ lib/features/shop_list/data/models/shop_list.dart
@@ -0,0 +1,82 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
+
+part 'shop_list.freezed.dart';
+part 'shop_list.g.dart';
+
+ShopList shopListFromJson(String rawJson) =>
+    ShopList.fromJson(json.decode(rawJson));
+
+@freezed
+class ShopList with _$ShopList {
+  const factory ShopList({
+    required List<ShopListData> shops,
+  }) = _ShopList;
+
+  factory ShopList.fromJson(Map<String, dynamic> json) =>
+      _$ShopListFromJson(json);
+}
+
+@freezed
+class ShopListData with _$ShopListData {
+  const factory ShopListData({
+    required String id,
+    required String slug,
+    @JsonKey(name: 'cover_url') String? coverUrl,
+    required String name,
+    // required Address address,
+    @JsonKey(name: 'phone_number') String? phoneNumber,
+    // required City city,
+    @JsonKey(name: 'average_rating') int? averageRating,
+    @JsonKey(name: 'review_count') int? reviewCount,
+    @JsonKey(name: 'activity_names') List<String>? activityNames,
+    @JsonKey(name: 'is_favourite') bool? isFavourite,
+    @JsonKey(name: 'distance_in_meters') int? distanceInMeters,
+  }) = _ShopListData;
+
+  factory ShopListData.fromJson(Map<String, dynamic> json) =>
+      _$ShopListDataFromJson(json);
+}
+
+@freezed
+class Address with _$Address {
+  const factory Address({
+    required String address1,
+    @JsonKey(name: 'zip_code') String? zipCode,
+    String? city,
+    String? country,
+    double? latitude,
+    double? longitude,
+  }) = _Address;
+
+  factory Address.fromJson(Map<String, dynamic> json) =>
+      _$AddressFromJson(json);
+}
+
+@freezed
+class City with _$City {
+  const factory City({
+    required String id,
+    String? slug,
+    required String name,
+    double? latitude,
+    double? longitude,
+    @JsonKey(name: 'zip_code') String? zipCode,
+    String? country,
+    @JsonKey(name: 'country_details') CountryDetails? countryDetails,
+  }) = _City;
+
+  factory City.fromJson(Map<String, dynamic> json) => _$CityFromJson(json);
+}
+
+@freezed
+class CountryDetails with _$CountryDetails {
+  const factory CountryDetails({
+    required String name,
+    @JsonKey(name: 'country_code') String? countryCode,
+    @JsonKey(name: 'language_code') String? languageCode,
+  }) = _CountryDetails;
+
+  factory CountryDetails.fromJson(Map<String, dynamic> json) =>
+      _$CountryDetailsFromJson(json);
+}
