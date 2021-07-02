0e59f3e9becb9b4ca6ff445dfab1b14120101a35
Albert Oboh <albertoboh7@gmail.com>
2021-07-03 00:24:07 +0100

    clean up main

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ring_shop_list/core/theme/custom_theme.dart';
 import 'service_locator.dart' as di;
 
 void main() async {
   //initialize dependency injection
   await di.init();
 
-  runApp(ProviderScope(child: ShoeList()));
+  runApp(ProviderScope(child: ShopList()));
 }
 
-class ShoeList extends StatelessWidget {
+class ShopList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title ?? ''),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Shop List',
+      theme: CustomTheme.themeData,
+      home: Container(),
     );
   }
 }
