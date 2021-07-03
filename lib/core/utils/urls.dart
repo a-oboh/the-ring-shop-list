fefe939a49c57743c54ffe6939d98380293ac0ca
Albert Oboh <albertoboh7@gmail.com>
2021-07-03 19:33:28 +0100

    edit url

--- lib/core/utils/urls.dart
+++ lib/core/utils/urls.dart
@@ -2,6 +2,9 @@ class ApiUrls {
   static const baseUrl = 'https://api.the-ring.io/customers/browse/shops';
 
   static String getShopsWithCity(
-          {required String city, int limit = 0, int offset = 0}) =>
-      '$baseUrl?city=$city&limit=$limit&offset=$offset';
+          {required String city,
+          int limit = 0,
+          int offset = 0,
+          int products = 5}) =>
+      '$baseUrl?city=$city&limit=$limit&offset=$offset&include[products]=$products&filter[selling_products]=true';
 }
