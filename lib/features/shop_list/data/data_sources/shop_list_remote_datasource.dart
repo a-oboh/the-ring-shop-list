a55087225cf0fe04b90a41a49c793153168486ef
Albert Oboh <albertoboh7@gmail.com>
2021-07-03 20:49:25 +0100

    implement fetching by city

---
+++ lib/features/shop_list/data/data_sources/shop_list_remote_datasource.dart
@@ -0,0 +1,42 @@
+import 'package:ring_shop_list/core/error/exception.dart';
+import 'package:ring_shop_list/core/utils/urls.dart';
+import 'package:http/http.dart' as http;
+import 'package:ring_shop_list/features/shop_list/data/models/shop_list_model.dart';
+
+abstract class ShopListRemoteDataSource {
+  Future<ShopListModel> getShopListFromCity(String city,
+      {int limit = 0, int offset = 0});
+  Future<ShopListModel> getShopListFromGeoData(double long, double lat,
+      {int limit = 0, int offset = 0});
+}
+
+class ShopListRemoteDataSourceImpl extends ShopListRemoteDataSource {
+  ShopListRemoteDataSourceImpl({required this.client});
+
+  final http.Client client;
+
+  @override
+  Future<ShopListModel> getShopListFromCity(String city,
+      {int limit = 0, int offset = 0}) async {
+    final response = await client.get(Uri.parse(
+        ApiUrls.getShopsWithCity(city: city, limit: limit, offset: offset)));
+
+    if (response.statusCode == 200) {
+      return shopListFromJson(response.body);
+    } else if (response.statusCode == 400) {
+      throw BadRequestException(
+          message:
+              // ignore: lines_longer_than_80_chars
+              'Something went wrong :(, please check that you inputed the correct city');
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<ShopListModel> getShopListFromGeoData(double long, double lat,
+      {int limit = 0, int offset = 0}) {
+    // TODO: implement getShopListFromGeoData
+    throw UnimplementedError();
+  }
+}
