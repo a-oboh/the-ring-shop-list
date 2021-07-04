29a35ee81876fd58beb3938c9bbd1aa006cc1434
Albert Oboh <albertoboh7@gmail.com>
2021-07-04 22:19:16 +0100

    minor changes

--- lib/core/utils/extensions.dart
+++ lib/core/utils/extensions.dart
@@ -9,3 +9,25 @@ extension MediaQueryExtension on BuildContext {
   double height(double value) => mediaQuery.size.height * value;
   double width(double value) => mediaQuery.size.width * value;
 }
+
+extension ImageKit on String {
+  String imgKitWidth(double width) {
+    // ignore: unnecessary_this
+    return this.replaceFirst(RegExp(r'(w-)\d*'), 'w-$width');
+  }
+
+  String imgKitSize(double width, double height) {
+    // ignore: unnecessary_this
+    return this
+        .replaceFirst(RegExp(r'(h-)\d*'), 'h-${height.toInt()}')
+        .replaceFirst(RegExp(r'(w-)\d*'), 'w-${width.toInt()}');
+  }
+
+  String imgKitBlur(double width, double height) {
+    // ignore: unnecessary_this
+    return this
+            .replaceFirst(RegExp(r'(h-)\d*'), 'h-${height.toInt()}')
+            .replaceFirst(RegExp(r'(w-)\d*'), 'w-${width.toInt()}') +
+        ',q-10,bl-10';
+  }
+}
--- lib/core/utils/urls.dart
+++ lib/core/utils/urls.dart
@@ -3,7 +3,7 @@ class ApiUrls {
 
   static String getShopsWithCity(
           {required String city,
-          int limit = 0,
+          int limit = 10,
           int offset = 0,
           int products = 5}) =>
       '$baseUrl?city=$city&limit=$limit&offset=$offset&include[products]=$products&filter[selling_products]=true';
